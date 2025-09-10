14df7a411b7827de392e4992e1b4d932a75b715a
vectinx <vectinx@yandex.ru>
2025-09-10 18:50:32 +0300

    Add code under version control

---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"389-ds-exporter/src/collectors"
+	"389-ds-exporter/src/config"
+	"389-ds-exporter/src/metrics"
+
+	"github.com/alecthomas/kingpin/v2"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/version"
+)
+
+// defaultHttpResponse function generates a standard HTML response for the exporter
+func defaultHttpResponse(metricsPath string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte(`
+		<html>
+			<head>
+				<title>389-ds-exporter</title>
+			</head>
+			<body>
+				<p>Metrics are <a href='` + metricsPath + `'>here</a></p>
+			</body>
+		</html>`))
+		if err != nil {
+			log.Printf("Error writing HTTP answer: %s", err)
+		}
+	}
+}
+
+func main() {
+	var (
+		configFilePath = kingpin.Flag("config", "Path to configuration file").
+				Default(":9389").
+				String()
+		listenAddress = kingpin.Flag("http.listen-address", "Address to listen on").
+				Default(":9389").
+				String()
+		metricsPath = kingpin.Flag("http.metrics-path", "Path to expose metrics").
+				Default("/metrics").
+				String()
+		httpReadTimeout = kingpin.Flag("http.read-timeout", "HTTP read timeout").
+				Default("5").Int()
+		httpWriteTimeout = kingpin.Flag("http.write-timeout", "HTTP write timeout").
+					Default("10").Int()
+		httpIdleimeout = kingpin.Flag("http.idle-timeout", "HTTP idle timeout").
+				Default("120").Int()
+	)
+
+	kingpin.Version(version.Print("ds_exporter"))
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
+
+	configuration := config.ReadConfig(*configFilePath)
+	log.Println(configuration)
+
+	dsMetricsRegistry := prometheus.NewRegistry()
+
+	dsMetricsRegistry.MustRegister(collectors.NewLdapEntryCollector(
+		"ds_exporter",
+		configuration.LdapServerUrl,
+		configuration.LdapBindDn,
+		configuration.LdapBindPw,
+		"cn=monitor",
+		metrics.GetLdapServerMetrics(),
+		prometheus.Labels{},
+	),
+	)
+
+	dsMetricsRegistry.MustRegister(collectors.NewLdapEntryCollector(
+		"ds_exporter",
+		configuration.LdapServerUrl,
+		configuration.LdapBindDn,
+		configuration.LdapBindPw,
+		"cn=snmp,cn=monitor",
+		metrics.GetLdapServerSnmpMetrics(),
+		prometheus.Labels{},
+	),
+	)
+
+	dsMetricsRegistry.MustRegister(collectors.NewLdapEntryCollector(
+		"ds_exporter",
+		configuration.LdapServerUrl,
+		configuration.LdapBindDn,
+		configuration.LdapBindPw,
+		"cn=monitor,cn=ldbm database,cn=plugins,cn=config",
+		metrics.GetLdapServerCacheMetrics(),
+		prometheus.Labels{},
+	),
+	)
+
+	for _, backend := range configuration.Backends {
+		dsMetricsRegistry.MustRegister(collectors.NewLdapEntryCollector(
+			"ds_exporter",
+			configuration.LdapServerUrl,
+			configuration.LdapBindDn,
+			configuration.LdapBindPw,
+			"cn=monitor,cn="+backend+",cn=ldbm database,cn=plugins,cn=config",
+			metrics.GetLdapBackendCaches(),
+			prometheus.Labels{"database": backend},
+		),
+		)
+	}
+
+	http.Handle(*metricsPath, promhttp.HandlerFor(dsMetricsRegistry, promhttp.HandlerOpts{}))
+	http.HandleFunc("/", defaultHttpResponse(*metricsPath))
+
+	server := &http.Server{
+		Addr:         *listenAddress,
+		Handler:      http.DefaultServeMux,
+		ReadTimeout:  time.Duration(*httpReadTimeout) * time.Second,
+		WriteTimeout: time.Duration(*httpWriteTimeout) * time.Second,
+		IdleTimeout:  time.Duration(*httpIdleimeout) * time.Second,
+	}
+
+	log.Fatal(server.ListenAndServe())
+}
---
+++ src/metrics/ldap_server_caches.go
@@ -0,0 +1,83 @@
+package metrics
+
+import (
+	"389-ds-exporter/src/collectors"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+// GetLdapServerCacheMetrics function returns map of attributes defining ldap server caches metrics.
+func GetLdapServerCacheMetrics() map[string]collectors.LdapMonitoredAttribute {
+	return map[string]collectors.LdapMonitoredAttribute{
+		"dbcachehits": {
+			LdapName: "dbcachehits",
+			Help:     "Number of requested pages found in the database",
+			Type:     prometheus.GaugeValue,
+		},
+		"dbcachetries": {
+			LdapName: "dbcachetries",
+			Help:     "Total number of cache lookups",
+			Type:     prometheus.GaugeValue,
+		},
+		"dbcachehitratio": {
+			LdapName: "dbcachehitratio",
+			Help:     "Percentage of requested pages found in the database cache",
+			Type:     prometheus.GaugeValue,
+		},
+		"dbcachepagein": {
+			LdapName: "dbcachepagein",
+			Help:     "Number of pages read into the database cache",
+			Type:     prometheus.GaugeValue,
+		},
+		"dbcachepageout": {
+			LdapName: "dbcachepageout",
+			Help:     "Number of pages written from the database cache to the backing file",
+			Type:     prometheus.GaugeValue,
+		},
+		"dbcacheroevict": {
+			LdapName: "dbcacheroevict",
+			Help:     "Number of clean pages forced from the cache",
+			Type:     prometheus.GaugeValue,
+		},
+		"dbcacherwevict": {
+			LdapName: "dbcacherwevict",
+			Help:     "Number of dirty pages forced from the cache",
+			Type:     prometheus.GaugeValue,
+		},
+		"normalizeddncachetries": {
+			LdapName: "normalizeddncachetries",
+			Help:     "Total number of cache lookups since the instance was started",
+			Type:     prometheus.GaugeValue,
+		},
+		"normalizeddncachehits": {
+			LdapName: "normalizeddncachehits",
+			Help:     "Normalized DNs found within the cache.",
+			Type:     prometheus.GaugeValue,
+		},
+		"normalizeddncachemisses": {
+			LdapName: "normalizeddncachemisses",
+			Help:     "Normalized DNs not found within the cache",
+			Type:     prometheus.GaugeValue,
+		},
+		"normalizeddncachehitratio": {
+			LdapName: "normalizeddncachehitratio",
+			Help:     "Percentage of the normalized DNs found in the cache",
+			Type:     prometheus.GaugeValue,
+		},
+		"currentnormalizeddncachesize": {
+			LdapName: "currentnormalizeddncachesize",
+			Help:     "Current size of the normalized DN cache in bytes",
+			Type:     prometheus.GaugeValue,
+		},
+		"maxnormalizeddncachesize": {
+			LdapName: "maxnormalizeddncachesize",
+			Help:     "Maximum size of NDn cache",
+			Type:     prometheus.GaugeValue,
+		},
+		"currentnormalizeddncachecount": {
+			LdapName: "currentnormalizeddncachecount",
+			Help:     "Number of normalized cached DNs",
+			Type:     prometheus.GaugeValue,
+		},
+	}
+}
